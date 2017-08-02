5d19f7b0ffec28a4404decc7c9773d00a2fbdacc
Kelly Selden <kellyselden@gmail.com>
2017-08-02 16:26:06 -0700

    get buildUrl off the prototype

--- addon/services/adapter.js
+++ addon/services/adapter.js
@@ -7,6 +7,19 @@ const {
   inject: { service }
 } = Ember;
 
+function buildUrl(adapter, url) {
+  let host = getWithDefault(adapter, 'host', 'http://localhost:4200');
+  let namespace = get(adapter, 'namespace');
+
+  let parts = [host];
+  if (namespace) {
+    parts.push(namespace);
+  }
+  parts.push(url);
+
+  return parts.join('/');
+}
+
 export default Service.extend({
   store: service(),
 
@@ -17,25 +30,12 @@ export default Service.extend({
     set(this, '_adapter', adapter);
   },
 
-  _buildUrl(adapter, url) {
-    let host = getWithDefault(adapter, 'host', 'http://localhost:4200');
-    let namespace = get(adapter, 'namespace');
-
-    let parts = [host];
-    if (namespace) {
-      parts.push(namespace);
-    }
-    parts.push(url);
-
-    return parts.join('/');
-  },
-
   ajax(url) {
     let adapter = get(this, '_adapter');
 
     let shouldIgnore = url.match(/^https?:\/\//);
     if (!shouldIgnore) {
-      url = this._buildUrl(adapter, url);
+      url = buildUrl(adapter, url);
     }
 
     let params = Array.prototype.slice.call(arguments).slice(1);

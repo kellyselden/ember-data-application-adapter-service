36d9d4a1d94e2b400ba44cec9213b4e1c56f01e2
Kelly Selden <kellyselden@gmail.com>
2018-09-06 14:43:02 +0100

    ember-cli-update (#52)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# ember-data-application-adapter-service
+ember-data-application-adapter-service
+==============================================================================
 
 [![Greenkeeper badge](https://badges.greenkeeper.io/kellyselden/ember-data-application-adapter-service.svg)](https://greenkeeper.io/)
 [![npm version](https://badge.fury.io/js/ember-data-application-adapter-service.svg)](https://badge.fury.io/js/ember-data-application-adapter-service)
@@ -6,6 +7,17 @@
 [![Ember Version](https://img.shields.io/badge/ember-1.13%2B-brightgreen.svg)](https://www.emberjs.com/)
 
 Send AJAX through your adapter without building the URL
+
+Installation
+------------------------------------------------------------------------------
+
+```
+ember install ember-data-application-adapter-service
+```
+
+
+Usage
+------------------------------------------------------------------------------
 
 Convert
 
@@ -33,8 +45,36 @@ actions: {
 }
 ```
 
-## Installation
 
-```sh
-ember install ember-data-application-adapter-service
-```
+Contributing
+------------------------------------------------------------------------------
+
+### Installation
+
+* `git clone <repository-url>`
+* `cd ember-data-application-adapter-service`
+* `npm install`
+
+### Linting
+
+* `npm run lint:hbs`
+* `npm run lint:js`
+* `npm run lint:js -- --fix`
+
+### Running tests
+
+* `ember test` – Runs the test suite on the current Ember version
+* `ember test --server` – Runs the test suite in "watch mode"
+* `ember try:each` – Runs the test suite against multiple Ember versions
+
+### Running the dummy application
+
+* `ember serve`
+* Visit the dummy application at [http://localhost:4200](http://localhost:4200).
+
+For more information on using ember-cli, visit [https://ember-cli.com/](https://ember-cli.com/).
+
+License
+------------------------------------------------------------------------------
+
+This project is licensed under the [MIT License](LICENSE.md).
--- tests/unit/services/adapter-test.js
+++ tests/unit/services/adapter-test.js
@@ -1,13 +1,16 @@
 import { Promise } from 'rsvp';
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
 
 let service;
 let adapter;
 let adapterForStub, ajaxStub;
 
-moduleFor('service:adapter', 'Unit | Service | adapter', {
-  beforeEach() {
+module('Unit | Service | adapter', function(hooks) {
+  setupTest(hooks);
+
+  hooks.beforeEach(function() {
     ajaxStub = sinon.stub().returns(Promise.resolve(12));
     adapter = {
       host: 'test-host',
@@ -16,62 +19,62 @@ moduleFor('service:adapter', 'Unit | Service | adapter', {
     };
     adapterForStub = sinon.stub().returns(adapter);
 
-    service = this.subject({
+    service = this.owner.factoryFor('service:adapter').create({
       store: {
         adapterFor: adapterForStub
       }
     });
-  }
-});
-
-test('grabs application adapter', function(assert) {
-  return service.ajax('test-url').then(() => {
-    assert.deepEqual(adapterForStub.args, [['application']]);
   });
-});
 
-test('calls adapter.ajax with full url', function(assert) {
-  return service.ajax('test-url').then(() => {
-    assert.deepEqual(ajaxStub.args, [['test-host/test-namespace/test-url']]);
+  test('grabs application adapter', function(assert) {
+    return service.ajax('test-url').then(() => {
+      assert.deepEqual(adapterForStub.args, [['application']]);
+    });
   });
-});
 
-test('calls adapter.ajax with remaining params', function(assert) {
-  return service.ajax('test-url', 23, 34).then(() => {
-    assert.deepEqual(ajaxStub.args, [['test-host/test-namespace/test-url', 23, 34]]);
+  test('calls adapter.ajax with full url', function(assert) {
+    return service.ajax('test-url').then(() => {
+      assert.deepEqual(ajaxStub.args, [['test-host/test-namespace/test-url']]);
+    });
   });
-});
 
-test('skips the logic if starts with http://', function(assert) {
-  return service.ajax('http://test-url').then(() => {
-    assert.deepEqual(ajaxStub.args, [['http://test-url']]);
+  test('calls adapter.ajax with remaining params', function(assert) {
+    return service.ajax('test-url', 23, 34).then(() => {
+      assert.deepEqual(ajaxStub.args, [['test-host/test-namespace/test-url', 23, 34]]);
+    });
   });
-});
 
-test('skips the logic if starts with https://', function(assert) {
-  return service.ajax('https://test-url').then(() => {
-    assert.deepEqual(ajaxStub.args, [['https://test-url']]);
+  test('skips the logic if starts with http://', function(assert) {
+    return service.ajax('http://test-url').then(() => {
+      assert.deepEqual(ajaxStub.args, [['http://test-url']]);
+    });
   });
-});
 
-test('returns adapter.ajax response', function(assert) {
-  return service.ajax('test-url').then(response => {
-    assert.strictEqual(response, 12);
+  test('skips the logic if starts with https://', function(assert) {
+    return service.ajax('https://test-url').then(() => {
+      assert.deepEqual(ajaxStub.args, [['https://test-url']]);
+    });
   });
-});
 
-test('uses a relative url if host not supplied', function(assert) {
-  delete adapter.host;
-
-  return service.ajax('test-url').then(() => {
-    assert.deepEqual(ajaxStub.args, [['/test-namespace/test-url']]);
+  test('returns adapter.ajax response', function(assert) {
+    return service.ajax('test-url').then(response => {
+      assert.strictEqual(response, 12);
+    });
   });
-});
 
-test('uses default namespace if not supplied', function(assert) {
-  delete adapter.namespace;
+  test('uses a relative url if host not supplied', function(assert) {
+    delete adapter.host;
 
-  return service.ajax('test-url').then(() => {
-    assert.deepEqual(ajaxStub.args, [['test-host/test-url']]);
+    return service.ajax('test-url').then(() => {
+      assert.deepEqual(ajaxStub.args, [['/test-namespace/test-url']]);
+    });
+  });
+
+  test('uses default namespace if not supplied', function(assert) {
+    delete adapter.namespace;
+
+    return service.ajax('test-url').then(() => {
+      assert.deepEqual(ajaxStub.args, [['test-host/test-url']]);
+    });
   });
 });

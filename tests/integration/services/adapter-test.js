c539959747521a4b242d60ee90555d0860a1da35
Kelly Selden <kellyselden@gmail.com>
2018-09-06 11:28:31 +0100

    ember-cli-update

--- tests/integration/services/adapter-test.js
+++ tests/integration/services/adapter-test.js
@@ -1,34 +1,37 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 import sinon from 'sinon';
 
 let server;
 let service;
 
-moduleFor('service:adapter', 'Integration | Service | adapter', {
-  integration: true,
-  beforeEach() {
+module('Integration | Service | adapter', function(hooks) {
+  setupTest(hooks);
+
+  hooks.beforeEach(function() {
     server = sinon.fakeServer.create();
     server.respondImmediately = true;
 
-    service = this.subject();
-  },
-  afterEach() {
-    server.restore();
-  }
-});
-
-test('calls ajax with full url', function(assert) {
-  server.respondWith('test-host/test-namespace/test-url', '[12]');
-
-  return service.ajax('test-url').then(response => {
-    assert.deepEqual(response, [12]);
+    service = this.owner.lookup('service:adapter');
   });
-});
 
-test('sends POST', function(assert) {
-  server.respondWith('POST', 'test-host/test-namespace/test-url', '[12]');
+  hooks.afterEach(function() {
+    server.restore();
+  });
 
-  return service.ajax('test-url', 'POST').then(response => {
-    assert.deepEqual(response, [12]);
+  test('calls ajax with full url', function(assert) {
+    server.respondWith('test-host/test-namespace/test-url', '[12]');
+
+    return service.ajax('test-url').then(response => {
+      assert.deepEqual(response, [12]);
+    });
+  });
+
+  test('sends POST', function(assert) {
+    server.respondWith('POST', 'test-host/test-namespace/test-url', '[12]');
+
+    return service.ajax('test-url', 'POST').then(response => {
+      assert.deepEqual(response, [12]);
+    });
   });
 });
